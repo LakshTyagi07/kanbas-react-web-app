85102664439ba11b403d7c0a76c194f0fe8d9f8d
LakshTyagi07 <75003870+LakshTyagi07@users.noreply.github.com>
2024-09-17 17:22:21 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import Lab1 from './Labs/Lab1';
+import Lab2 from './Labs/Lab2';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Lab1/>
+      <Lab2/>
     </div>
   );
 }
